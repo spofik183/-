9d3c1be0c586022ce8536dee9bdc9a256a537d5c
spofik183 <danilaplayspb@gmail.com>
2026-09-25 16:27:57 +0300

    Delete src directory

--- src/providers/payment-provider.mjs
+++
@@ -1,6 +0,0 @@
-export class PaymentProvider {
-  async getClientConfig() { throw new Error('Not implemented'); }
-  async createPayout(_payload) { throw new Error('Not implemented'); }
-  async createTopup(_payload) { throw new Error('Not implemented'); }
-  async getTopupStatus(_paymentId) { throw new Error('Not implemented'); }
-}
